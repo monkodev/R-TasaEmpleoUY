17c383e5801d85dae43e1c770f200865defb3729
MonKoDev Technologies <gmalugani@gmail.com>
2019-09-26 07:20:09 -0300

    Install ggplot + cambia fuente datos

--- script1.R
+++ script1.R
@@ -1,3 +1,6 @@
 ### importar datos de https://catalogodatos.gub.uy datos abiertos
 
-df_train<-read.csv("_tasa_de_empleo.xlsx")
+df_train<-read.csv("tasa_de_empleo_segun.csv")
+
+install.packages("ggplot2")
+library("ggplot2")
